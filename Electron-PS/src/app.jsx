7df90f198553526ff8e8405877410b5cecf8e73e
Nyqvuist <shiraziahassan@gmail.com>
2025-02-05 22:06:47 -0500

    initial setup

---
+++ Electron-PS/src/app.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import './components/main.jsx';
+
+const root = createRoot(document.body);
+root.render(<main/>);
--- Electron-PS/src/main.js
+++ Electron-PS/src/main.js
@@ -15,6 +15,7 @@ const createWindow = () => {
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
+    autoHideMenuBar: true,
   });
 
   // and load the index.html of the app.

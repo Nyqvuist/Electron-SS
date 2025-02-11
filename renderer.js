15a521e38fde30cf0b285ac3eb04614c2f783f78
Nyqvuist-W <shiraziahassan@gmail.com>
2025-02-11 11:46:54 -0500

    added dynamic path and multi button

--- renderer.js
+++ renderer.js
@@ -1,8 +1,18 @@
-const button = document.getElementById('button')
-const path = document.getElementById('button').value
+const buttons = document.querySelectorAll("button")
 
-button.addEventListener('click', e=> {
+buttons.forEach(button => {
+   button.addEventListener("click", e=> {
+      let id = e.target.id;
+      let path = document.getElementById(`${id}`).value;
+      let data = window.scriptCalls.button(path);
+      document.getElementById('output').textContent = data;
+   })
+})
+
+/*
+btnContainer.addEventListener('click', e=> {
    let data = window.scriptCalls.button(path);
    document.getElementById('output').textContent = data;
    console.log(data);
 })
+   */
